b090ca27451fb0764a4ab2aacf214f533792c2c0
Angle0o <753355530@qq.com>
2017-08-05 11:37:02 +0800

    Initial commit

---
+++ app/src/main/java/com/jinlong/channel/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jinlong.channel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 讲解多渠道打包的案例
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
